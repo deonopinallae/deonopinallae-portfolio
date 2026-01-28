50cdb9758a00897dff3083baf24905b018d58f40
Alisa Tishchenko <deonopinallae@yandex.ru>
2026-01-28 10:59:06 +0300

    Updates

--- _next/static/GQ3Z3RXzD8LsaFo2YpcwB/_buildManifest.js
+++ _next/static/GQ3Z3RXzD8LsaFo2YpcwB/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/deonopinallae-portfolio//_next/:path+",
-        "destination": "/deonopinallae-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
