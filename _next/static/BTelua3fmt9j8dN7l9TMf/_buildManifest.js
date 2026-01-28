b621726b53627dbc5f820d4561969881043dd201
Alisa Tishchenko <deonopinallae@yandex.ru>
2026-01-28 10:57:34 +0300

    Updates

--- _next/static/BTelua3fmt9j8dN7l9TMf/_buildManifest.js
+++ _next/static/BTelua3fmt9j8dN7l9TMf/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/deonopinallae-portfolio//_next/:path+",
+        "destination": "/deonopinallae-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
